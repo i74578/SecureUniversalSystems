8f6a928f4a500d1858827474c584194237150f48
s224350 <s224350@student.dtu.dk>
2024-01-11 10:23:20 +0100

    ability to also remove members from the list, and first steps of PIN authentication

--- dynamicArrayTest/dynamicArrayTest.ino
+++ dynamicArrayTest/dynamicArrayTest.ino
@@ -1,80 +1,121 @@
-struct member {
+struct employee {
   byte NUID[4] = {0x00, 0x00, 0x00, 0x00};
   int PIN = 0000;
 };
 
 const int arrLength = 10;
-member arr[arrLength];
+int employeeCount = 0;
+employee employees[arrLength];
+employee currentlyLoggingIn; //this is set by startLogin() when a valid ID is scanned. It is then used by enterPIN() to see if the PIN matches.
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello world!");
-  initArray(arr);
-  byte temp[] = {0x01, 0x02, 0x23, 0xFF};
-  byte temp2[] = {0x21, 0xF2, 0xAB, 0xDD};
-  byte temp3[] = {0x21, 0xF2, 0xAB, 0xDF};
-  addMember(arr, temp, 1234);
-  addMember(arr, temp2, 5678);
-  if (checkAccess(arr, temp)) {
-    Serial.println("Yes!!");
-  } else {
-    Serial.println("defeat...");
-  }
-  if (checkAccess(arr, temp3)) {
-    Serial.println("wrong");
-  } else {
-    Serial.println("yes!");
-  }
+  initArray(employees);
+  resetCurrentlyLoggingIn();
+  byte UID1[] = {0x01, 0x02, 0x23, 0xFF};
+  byte UID2[] = {0x21, 0xF2, 0xAB, 0xDD};
+  byte UID3[] = {0x21, 0xF2, 0xAB, 0xDF};
+  addMember(employees, UID1, 1234);
+  addMember(employees, UID2, 5678);
+
+  startLogin()
+
+  
 
 }
 
-void loop() {
+void loop() { 
 
 }
 
-void initArray(member m[]) {
+void initArray(employee e[]) {
   for (int i = 0; i < arrLength; i++) {
-    m[i].NUID[0] = 0x00;
-    m[i].NUID[1] = 0x00;
-    m[i].NUID[2] = 0x00;
-    m[i].NUID[3] = 0x00;
-    m[i].PIN = 0;
+    e[i].NUID[0] = 0x00;
+    e[i].NUID[1] = 0x00;
+    e[i].NUID[2] = 0x00;
+    e[i].NUID[3] = 0x00;
+    e[i].PIN = 0;
   }
 }
 
-void addMember(member m[], byte newNUID[], int newPIN) {
+void addMember(employee e[], byte newNUID[], int newPIN) {
   for (int i = 0; i < arrLength; i++) {
-    if (m[i].NUID[0] == 0x00 &&
-        m[i].NUID[1] == 0x00 &&
-        m[i].NUID[2] == 0x00 &&
-        m[i].NUID[3] == 0x00) {
+    if (e[i].NUID[0] == 0x00 &&
+        e[i].NUID[1] == 0x00 &&
+        e[i].NUID[2] == 0x00 &&
+        e[i].NUID[3] == 0x00) {
       // empty entry found
-      m[i].NUID[0] = newNUID[0];
-      m[i].NUID[1] = newNUID[1];
-      m[i].NUID[2] = newNUID[2];
-      m[i].NUID[3] = newNUID[3];
-      m[i].PIN = newPIN;
+      e[i].NUID[0] = newNUID[0];
+      e[i].NUID[1] = newNUID[1];
+      e[i].NUID[2] = newNUID[2];
+      e[i].NUID[3] = newNUID[3];
+      e[i].PIN = newPIN;
       return;
     }
   }
 }
-bool checkAccess(member m[], byte NUID[]) {
+
+void removeMember(employee e[], byte NUID[]) {
+for (int i = 0; i < arrLength; i++) {
+    if (e[i].NUID[0] == NUID[0] &&
+        e[i].NUID[1] == NUID[1] &&
+        e[i].NUID[2] == NUID[2] &&
+        e[i].NUID[3] == NUID[3]) {
+      // matching entry found
+      e[i].NUID[0] = 0x00;
+      e[i].NUID[1] = 0x00;
+      e[i].NUID[2] = 0x00;
+      e[i].NUID[3] = 0x00;
+      e[i].PIN = 0;
+      return;
+    }
+  }
+
+
+
+
+}
+
+bool startLogin(employee e[], byte NUID[]) {
+
+// When user has scanned their card and entered the NUID, the system checks whether it exists in the list of people with access.
+// If it does, this function returns true and sets 
 
   if (NUID[0] == 0x00 &&
       NUID[1] == 0x00 &&
       NUID[2] == 0x00 &&
       NUID[3] == 0x00) {
     // 00 00 00 00 is used for empty entries in the list, so it's not a valid NUID in our case.
+    resetCurrentlyLoggingIn();
     return false;
   }
 
   for (int i = 0; i < arrLength; i++) {
-    if (m[i].NUID[0] == NUID[0] &&
-        m[i].NUID[1] == NUID[1] &&
-        m[i].NUID[2] == NUID[2] &&
-        m[i].NUID[3] == NUID[3]) {
+    if (e[i].NUID[0] == NUID[0] &&
+        e[i].NUID[1] == NUID[1] &&
+        e[i].NUID[2] == NUID[2] &&
+        e[i].NUID[3] == NUID[3]) {
+      currentlyLoggingIn = e[i];
       return true;
     }
   }
+  resetCurrentlyLoggingIn();
   return false;
+}
+
+bool enterPIN(int PIN) {
+
+
+
+}
+
+void resetCurrentlyLoggingIn() {
+  //resets the value of currentlyLoggingIn to one of all zeroes.
+  currentlyLoggingIn.NUID[0] = 0x00;
+  currentlyLoggingIn.NUID[1] = 0x00;
+  currentlyLoggingIn.NUID[2] = 0x00;
+  currentlyLoggingIn.NUID[3] = 0x00;
+  currentlyLoggingIn.PIN = 0;
+
 }
